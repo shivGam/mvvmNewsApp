fb55c162bacbdb37150654f1f51c9fd039a2af11
shivGam <shivdattjangam2003@gmail.com>
2024-12-03 03:58:50 +0530

    Removed Secrets

--- app/src/main/java/com/androiddevs/mvvmnewsapp/util/Contants.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Contants.kt
@@ -2,7 +2,7 @@ package com.androiddevs.mvvmnewsapp.util
 
 class Contants {
     companion object{
-        const val API_KEY="b725d8b3d75f4272a9df460c7d2ba144"
+        const val API_KEY="Add your own Key via newsapiorg"
         const val BASE_URL="https://newsapi.org"
         const val TIME_DELAY = 500L
         const val QUERY_PAGE_ITEM = 20
